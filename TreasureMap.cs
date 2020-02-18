964755347feb3c06d0327b1f1bd278af06738cd4
AndreySasin0 <andreysasin00@yandex.ru>
2020-02-19 02:16:36 +0300

    Update TreasureMap.cs
    Changed struct Point2d to class. Redefined equals for it. Added class Node to build a path. Added some methods to calculate the optimised path with a* algorithm. Does not work if try to filter points with river line. Does not choose optimised exit from base, does not optimised for case with two treasures.

--- TreasureMap.cs
+++ TreasureMap.cs
@@ -1,15 +1,24 @@
-﻿using System;
+﻿#nullable enable
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 
 namespace FLS_Task1
 {
-    struct Point2D
+    class Point2D
     {
         public Point2D(int x, int y)
         {
             X = x;
             Y = y;
+        }
+
+        public Point2D()
+        {
+            X = 0;
+            Y = 0;
         }
 
         public int X { get; }
@@ -20,11 +29,46 @@ namespace FLS_Task1
         {
             return X + " " + Y;
         }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Point2D other && Equals(other);
+        }
+
+        public bool Equals(Point2D other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
+
+        public static bool operator ==(Point2D left, Point2D right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Point2D left, Point2D right)
+        {
+            return !left.Equals(right);
+        }
     }
 
+    class Node
+    {
+        public Point2D Position { get; set; }
+        public int PathLengthFromStart { get; set; }
+        public Node PrevPosition { get; set; }
+        public int HeuristicEstimatedPathLength { get; set; } //approx distance to finish
+        public int EstimateFullPathLength => this.PathLengthFromStart + this.HeuristicEstimatedPathLength;
+    }
 
     class TreasureMap
     {
+        private const int Cost = 1; //cost of one point move
+
         static void DrawLine(Point2D firstCrd, Point2D secondCrd, List<Point2D> lstRiv)
         {
             var dx = secondCrd.X - firstCrd.X;
@@ -84,9 +128,98 @@ namespace FLS_Task1
             }
         }
 
+        static int GetHeuristicPathLength(Point2D from, Point2D to)
+        {
+            return Math.Abs(from.X - to.X) + Math.Abs(from.Y - to.Y);
+        }
+
+
+        static Collection<Node> GetNearNodes(Node node, Point2D finish, int[,] field, List<Point2D> lstRiv)
+        {
+            var result = new Collection<Node>();
+            var nearestPoints = new Point2D[4];
+            nearestPoints[0] = new Point2D(node.Position.X + 1, node.Position.Y);
+            nearestPoints[1] = new Point2D(node.Position.X - 1, node.Position.Y);
+            nearestPoints[2] = new Point2D(node.Position.X, node.Position.Y + 1);
+            nearestPoints[3] = new Point2D(node.Position.X, node.Position.Y - 1);
+
+            foreach (var point in nearestPoints)
+            {
+                if (point.X < 0 || point.X >= field.GetLength(0)) continue;
+                if (point.Y < 0 || point.Y >= field.GetLength(1)) continue;
+                //nullreference exception if I try to filter points with the lstRiv
+                // if (lstRiv.Any(rivPoint=>rivPoint.X == point.X && rivPoint.Y == point.Y)) continue; //COMMENT IT TO START ACTUAL BUILD
+                var neighbour = new Node()
+                {
+                    Position = point,
+                    PrevPosition = node,
+                    PathLengthFromStart = node.PathLengthFromStart + Cost,
+                    HeuristicEstimatedPathLength = GetHeuristicPathLength(point, finish)
+                };
+                result.Add(neighbour);
+            }
+
+            return result;
+        }
+
+        static List<Point2D> GetPathForNode(Node node)
+        {
+            var result = new List<Point2D>();
+            var currentNode = node;
+            while (currentNode != null)
+            {
+                result.Add(currentNode.Position);
+                currentNode = currentNode.PrevPosition;
+            }
+
+            result.Reverse();
+            return result;
+        }
+
+        static List<Point2D> FindPath(int[,] field, Point2D start, Point2D finish, List<Point2D> lstRiv)
+        {
+            var closedSet = new Collection<Node>();
+            var openedSet = new Collection<Node>();
+            var beginning = new Node()
+            {
+                Position = start,
+                PrevPosition = null,
+                PathLengthFromStart = 0,
+                HeuristicEstimatedPathLength = GetHeuristicPathLength(start, finish)
+            };
+            openedSet.Add(beginning);
+            while (openedSet.Count > 0)
+            {
+                var currentNode =
+                    openedSet.OrderBy(node => node.EstimateFullPathLength)
+                        .First(); //choose point with the lowest Full Length
+                if (currentNode.Position.Equals(finish)) return GetPathForNode(currentNode);
+                openedSet.Remove((currentNode));
+                closedSet.Add(currentNode);
+                foreach (var neighbour in GetNearNodes(currentNode, finish, field, lstRiv))
+                {
+                    if (closedSet.Count(node => node.Position.Equals(neighbour.Position)) > 0)
+                        continue; //if point is already in closedSet - skip it
+                    var openNode =
+                        openedSet.FirstOrDefault(node =>
+                            node.Position.Equals(neighbour
+                                .Position)); //add point to openedSet, counting path from it to finish
+                    if (openNode == null)
+                        openedSet.Add(neighbour);
+                    else if (openNode.PathLengthFromStart > neighbour.PathLengthFromStart)
+                    {
+                        openNode.PrevPosition = currentNode;
+                        openNode.PathLengthFromStart = neighbour.PathLengthFromStart;
+                    }
+                }
+            }
+
+            return null;
+        }
+
         static void Main(string[] args)
         {
-            var filePath = "Map10.txt";
+            const string filePath = "Map1.txt"; //ENTER THE PATH TO THE MAP HERE
             var sr = new StreamReader(filePath);
             //not supported on Mac OS
             //Console.SetWindowSize(columns, rows);
@@ -172,8 +305,22 @@ namespace FLS_Task1
                 }
             }
 
+            var field = new int [60, 40];
             Console.SetCursorPosition(bridge.X, bridge.Y);
             Console.WriteLine('#');
+            if (FindPath(field, lstBase[0], treasure, lstRiver).Count > 0)
+            {
+                foreach (var node in FindPath(field, lstBase[0], treasure, lstRiver))
+                {
+                    Console.SetCursorPosition(node.X, node.Y);
+                    Console.Write('%');
+                }
+            }
+            else
+            {
+                Console.WriteLine("There is no appropriate path");
+            }
+
             Console.ReadLine();
         }
     }
